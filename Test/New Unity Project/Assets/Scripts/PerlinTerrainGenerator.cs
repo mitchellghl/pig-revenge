027f6d09e7b7303eaeefffe08b6eab9d3f017d1f
mitchellghl <46975869+mitchellghl@users.noreply.github.com>
2019-10-09 03:41:45 -0400

    assignment 2 (need to move)

---
+++ Test/New Unity Project/Assets/Scripts/PerlinTerrainGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinTerrainGenerator : MonoBehaviour //Used in perlin noise scene
+{
+    Terrain terrain;
+    [RangeAttribute(1f, 20f)]
+    public float flatness = 1f;
+    [RangeAttribute(1f, 20f)]
+    public float frequency = 1f;
+    [RangeAttribute(1, 10)]
+    public int octaves = 8;
+
+    private float heightOffset = 0f; //used for infinite scrolling
+    private float widthOffset = 0f; //used for infinite scrolling
+
+    void Start()
+    {
+        terrain = GetComponent<Terrain>(); //get the terrain component
+    }
+
+    void Update()
+    {
+        float[,] heightmap = terrain.terrainData.GetHeights(0, 0, terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight); //create array of heights
+
+        heightOffset += (Input.GetAxis("Vertical") / 5f); //if left/right is pressed, increase/decrease offset
+        widthOffset += (Input.GetAxis("Horizontal") / 5f); //if up/down is pressed, increase/decrease offset
+
+        for (int i = 0; i < terrain.terrainData.heightmapHeight; ++i)
+        {
+            for (int j = 0; j < terrain.terrainData.heightmapWidth; ++j) //for every point in the heighmap...
+            {
+                float x = i / (float)terrain.terrainData.heightmapHeight;
+                float y = j / (float)terrain.terrainData.heightmapWidth;
+
+                float height = 0f;
+                float current_frequency = frequency;
+                float amplitude = 1;
+                for (int z = 0; z < octaves; ++z)
+                {
+                    height = height + Mathf.PerlinNoise(x * current_frequency + heightOffset, y * current_frequency + widthOffset) * amplitude; //...randomly generate height (and modify using offset)
+                    amplitude /= 2; //create erosion effect
+                    current_frequency *= 2;
+                }
+                heightmap[i, j] = height / flatness;
+            }
+        }
+        terrain.terrainData.SetHeights(0, 0, heightmap); //actually set the heights in the terrain
+    }
+}
---
+++ Test/New Unity Project/Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TerrainGenerator : MonoBehaviour //Used in heigh mapping scene
+{
+    Terrain terrain;
+    [RangeAttribute(1f,20f)]
+    public float flatness = 1f;
+    [RangeAttribute(1f,20f)]
+    Texture2D heightmap2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrain = GetComponent<Terrain>(); //get terrain component
+        heightmap2D = new Texture2D(terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight); //make a texture for the terrain map
+        heightmap2D.LoadImage(File.ReadAllBytes("Assets/MountHood.jpg")); //load the Mt. Hood image into the heightmap
+
+        float[,] heightmap = terrain.terrainData.GetHeights(0,0, terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight);
+
+        for(int i=0; i < terrain.terrainData.heightmapHeight; ++i)
+        {
+            for (int j=0; j < terrain.terrainData.heightmapWidth; ++j)
+            {
+                float x = i / (float) terrain.terrainData.heightmapHeight;
+                float y = j / (float) terrain.terrainData.heightmapWidth;
+                float height = heightmap2D.GetPixel(i,j).b; //set the height of each point to the height dictated in the Mt. Hood image
+
+                heightmap[i, j] = height/flatness;
+            }
+        }
+        terrain.terrainData.SetHeights(0, 0, heightmap); //set the height in the terrain
+
+    }
+}
